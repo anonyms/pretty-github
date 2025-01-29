bb3ee90c583e12d25ae768fe724cb6bf0519bf21
anonyms <xxyy.signups@gmail.com>
2025-01-29 19:43:59 +0100

    Final changes?

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,13 @@ WORKDIR /app
 # Install Git (needed for some dependencies)
 RUN apt-get update && apt-get install -y git && rm -rf /var/lib/apt/lists/*
 
+# Set Git user details using environment variables
+ARG GIT_USER
+ARG GIT_EMAIL
+RUN echo "Git User: $GIT_USER" && echo "Git Email: $GIT_EMAIL"  # Print the variables
+RUN git config --global user.name "$GIT_USER" \
+    && git config --global user.email "$GIT_EMAIL"
+
 # Copy the current directory contents into the container
 COPY . /app
 
--- make-commits.py
+++ make-commits.py
@@ -30,7 +30,7 @@ def create_commit(date):
     # Remove the file
     os.remove(filename)
 
-def update_contributions(start_date):
+def update_contributions(start_date,github_username, github_url):
     current_date = start_date
     for row in pattern:
         for char in row:
@@ -40,19 +40,26 @@ def update_contributions(start_date):
         current_date += timedelta(days=1)  # Move to the next row
 
     # Push the commits to the repository
-    subprocess.run(["git", "push"], check=True)
+    # Create the URL with the token embedded
+
+    token = input("Enter GitHub Token: ")
+    repo_with_token_url = f"https://{github_username}:{token}@{github_url[8:]}"  # Removes "https://"
+
+    # Run the git push command with the updated URL
+    subprocess.run(["git", "push", repo_with_token_url], check=True)
 
 if __name__ == "__main__":
     # Get start date from the user
     start_date_str = input("Enter the start date (YYYY-MM-DD): ")
     start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
 
-    github_username = input("GitHub User Name")
-    github_repo = input("Enter GitHub Repo for this user name ")
+    github_username = input("GitHub User Name: ")
+    github_repo = input("Enter GitHub Repo for this user name: ")
+    github_url = "https://github.com/"+github_username+"/"+github_repo+".git"
 
     # Initialize the git repository if it doesn't exist
     if not os.path.exists(".git"):
         subprocess.run(["git", "init"], check=True)
         subprocess.run(["git", "remote", "add", "origin", "https://github.com/"+github_username+"/"+github_repo+".git"], check=True)
 
-    update_contributions(start_date)
+    update_contributions(start_date, github_username, github_url)
